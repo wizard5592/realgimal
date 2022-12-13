a54ef61680dd68b2cb0312b6a58436873d53039f
wizard5592 <wizard5592@naver.com>
2022-12-13 19:58:21 +0900

    해결해결해결해결해결해결해결해결해결해결해결해결해결해결해결

--- Particle.js
+++ Particle.js
@@ -26,13 +26,13 @@ class Particle {
 
   edge(){
     if(this.pos.y > height-10 || this.pos.y < 10){
-      ParticleSystem.origin = createVector(this.pos.x, this.pos.y);
+      system.origin = createVector(this.pos.x, this.pos.y);
       this.vel.y = this.vel.y * -1;  
       system.addParticle();
       this.count--;
     }  
     if(this.pos.x > width - 10 || this.pos.x < 10){
-      ParticleSystem.origin = createVector(this.pos.x, this.pos.y);
+      system.origin = createVector(this.pos.x, this.pos.y);
       this.vel.x = this.vel.x * -1; 
       system.addParticle();
       this.count--;
