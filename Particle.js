c816b7f456ed016b6a33ab9e2176466841b94453
wizard5592 <wizard5592@naver.com>
2022-12-13 22:04:00 +0900

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,15 +1,10 @@
 class Particle {
-  constructor(position, changex,changey, ck){
+  constructor(position){
     this.vel = createVector(random(-1, 1), random(-1, 1));
     this.pos = position.copy();
-    this.ckvel = createVector(changex,changey);
     
     this.r = 20;
     this.count = 20;
-    if(ck > 0){
-      this.vel.x = this.ckvel.x * -1;
-      this.vel.y = this.ckvel.y * -1; 
-    }
   }
 
   run(){
@@ -30,28 +25,28 @@ class Particle {
 
   edge(){
     if(this.pos.y > height-10){
-      system.origin = createVector(this.pos.x, this.pos.y - 1);
+      system.origin = createVector(this.pos.x, this.pos.y - 1 );
       print("1",this.ckvel);
       this.vel.y = this.vel.y * -1;  
-      system.addParticle(this.vel.x,this.vel.y);
+      system.addParticle();
       this.count--;
     }  
      if( this.pos.y < 10){
       system.origin = createVector(this.pos.x, this.pos.y + 1);
       this.vel.y = this.vel.y * -1;  
-      system.addParticle(this.vel.x,this.vel.y);
+      system.addParticle();
       this.count--;
     }  
     if(this.pos.x > width - 10){
       system.origin = createVector(this.pos.x - 1, this.pos.y);
       this.vel.x = this.vel.x * -1; 
-      system.addParticle(this.vel.x,this.vel.y);
+      system.addParticle();
       this.count--;
     } 
     if(this.pos.x < 10){
       system.origin = createVector(this.pos.x + 1, this.pos.y);
       this.vel.x = this.vel.x * -1; 
-      system.addParticle(this.vel.x,this.vel.y);
+      system.addParticle();
       this.count--;
     }
   }
